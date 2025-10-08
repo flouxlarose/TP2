0034987cf444828cabb4dfd9ac8e7f42813989fd
felix <larosefelixb@gmail.com>
2025-10-08 15:18:29 -0400

    init

--- src/Borne.java
+++ src/Borne.java
@@ -1,2 +1,5 @@
 public class Borne {
+    private String place;
+    private int banque;     // total en Cents
+    private Transaction transactionCourante = null;
 }
--- src/CarteCredit.java
+++ src/CarteCredit.java
@@ -1,2 +1,7 @@
-public class CarteCredit {
+import java.time.YearMonth;
+
+public class CarteCredit {      // c'est une carte de debit lol
+    private String numero;
+    private YearMonth exp;
+    private int solde;  // argent qui reste sur la carte
 }
