29cef44d69ca02d9765024158c9e99744f7c943a
filou <larosefelixb@gmail.com>
2025-10-17 23:37:00 -0400

    zebiii

--- src/Borne.java
+++ src/Borne.java
@@ -1,5 +1,18 @@
 public class Borne {
-    private String place;
+    private String place = "NONE";
     private int banque;     // total en Cents
-    private Transaction transactionCourante = null;
+    private Transaction transactionCourante;
+
+    void Borne (String place) {
+        setPlace(place);
+        banque = 0;
+        transactionCourante = null;
+    }
+
+    private void setPlace(String place) {   // besoin d'etre private?
+        if (place.matches("^(G|SQ)\\d{3}$")) {
+            this.place = place;
+        }
+
+    }
 }
